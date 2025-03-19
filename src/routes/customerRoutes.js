11d9ad5b1320dbc17e3054075374ba741d18153b
Suleman <i228807@nu.edu.pk>
2025-03-19 05:43:34 +0500

    changed

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const customerController = require("../controllers/CustomerController");
+const customerController = require("../controllers/customerController");
 const authMiddleware = require("../middlewares/authMiddleware");
 // GET all products
 router.get("/products", customerController.getAllProducts);
